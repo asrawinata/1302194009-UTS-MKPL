e3c01a678c198aa2c4b514ee37e5bef590536b48
asrawinata <asra.winata@icloud.com>
2022-04-20 21:08:08 +0700

    Updae employee.java
    Menghapus variabel addresss & gender, variabel ini tidak digunakan didalam code. variable yang tak digunakan termasuk dalam smells Dead Code

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -11,7 +11,6 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String idNumber;
-	private String address;
 	
 	private int yearJoined;
 	private int monthJoined;
@@ -19,7 +18,6 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,17 +29,16 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
-		this.address = address;
 		this.yearJoined = yearJoined;
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
-		this.gender = gender;
+
 		
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
